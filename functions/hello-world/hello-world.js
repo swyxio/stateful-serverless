84eea107f5a4564eb969e47431d7fa07e670eb4f
swyx <shawnthe1@gmail.com>
2020-02-20 23:24:39 -0500

    init

--- functions/hello-world/hello-world.js
+++ functions/hello-world/hello-world.js
@@ -1,15 +1,13 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+var abc = abc || 1;
 exports.handler = async (event, context) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    abc += 1;
+    const subject = event.queryStringParameters.name || 'World';
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` })
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
+      body: JSON.stringify({ message: `Hello ${subject} ${abc}` })
+    };
   } catch (err) {
-    return { statusCode: 500, body: err.toString() }
+    return { statusCode: 500, body: err.toString() };
   }
-}
+};
