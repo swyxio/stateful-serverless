d39bfebef436c5090e14efe71e1ad01d9bbf8cf1
swyx <shawnthe1@gmail.com>
2020-02-21 00:01:41 -0500

    rate limit

---
+++ functions/rate-limiting.js
@@ -0,0 +1,18 @@
+let count = 0;
+let firstInvoke = new Date();
+exports.handler = async (event, context) => {
+  let currentInvoke = new Date();
+  let diff = currentInvoke - firstInvoke;
+  if (diff < 5000) count += 1;
+  if (count > 3) {
+    return {
+      statusCode: 429,
+      body: JSON.stringify({ message: `Too many requests! ${count}` })
+    };
+  } else {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello, count is ${count}` })
+    };
+  }
+};
